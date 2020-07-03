13729e656b0023551c108df15518903e3e893340
MoussaAKHYAR <amrane93.mb@gmail.com>
2020-07-03 22:36:00 +0000

    new commit

--- src/sn/isi/appgestionsalleinformatique/ui/AdminController.java
+++ src/sn/isi/appgestionsalleinformatique/ui/AdminController.java
@@ -1,4 +1,23 @@
 package sn.isi.appgestionsalleinformatique.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AdminController {
+    @FXML
+    void ajouter(ActionEvent event){
+
+    }
+
+    @FXML
+    void supprimer(ActionEvent event){
+
+    }
+
+    @FXML
+    void modifier(ActionEvent event){
+
+    }
+
+
 }
--- src/sn/isi/appgestionsalleinformatique/ui/MachineController.java
+++ src/sn/isi/appgestionsalleinformatique/ui/MachineController.java
@@ -1,11 +1,33 @@
 package sn.isi.appgestionsalleinformatique.ui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MachineController implements Initializable {
+    @FXML
+    void ajouter(ActionEvent event){
+
+    }
+
+    @FXML
+    void supprimer(ActionEvent event){
+
+    }
+
+    @FXML
+    void modifier(ActionEvent event){
+
+    }
+
+    @FXML
+    void annuler(ActionEvent event){
+
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
